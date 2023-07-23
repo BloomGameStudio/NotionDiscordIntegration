bdc5deb5b53c96a1db928b8b62c9c59a2e1ded1c
Balu <baluproton+gitlab@protonmail.com>
2023-07-23 13:07:25 +0200

    Notion get page title

--- utils/notion_utils.py
+++ utils/notion_utils.py
@@ -12,3 +12,24 @@ def get_user_by_id(id):
 def get_username_by_id(id):
     name = get_user_by_id(id).get("name")
     return name
+
+def get_page_title(page):
+    _properties = page.get("properties")
+    # pprint(f"properties: {_properties}")
+
+    _Name = _properties.get("Page")
+    # pprint(f"Name:{_Name}")
+
+    _title = _Name.get("title")[0]
+    # pprint(f"_title:{_title}")
+
+    _plain_text = _title.get("plain_text")
+    # pprint(f"_plain_text:{_plain_text}")
+
+    _text = _title.get("text")
+    # pprint(f"_text:{_text}")
+
+    title = _text.get("content")
+    # pprint(f"title:{title}")
+
+    return title
