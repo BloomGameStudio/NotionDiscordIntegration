cb4e0a9f5909f5d8ba91209d0164f08e1f86956e
Balu <baluproton+gitlab@protonmail.com>
2023-07-23 12:57:04 +0200

    Get Notion Username By ID

--- utils/notion_utils.py
+++ utils/notion_utils.py
@@ -7,3 +7,8 @@ notion = Client(auth=os.environ["NOTION_TOKEN"])
 def get_user_by_id(id):
     user = notion.users.retrieve(id)
     return user
+
+
+def get_username_by_id(id):
+    name = get_user_by_id(id).get("name")
+    return name
