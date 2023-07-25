2cccb7c871b0e3f4d2dca7544cefce039f9682d8
Balu <baluproton+gitlab@protonmail.com>
2023-07-25 20:55:26 +0200

    Custom Logger Setup

---
+++ my_logger.py
@@ -0,0 +1,14 @@
+import logging
+import sys
+
+logging.basicConfig(
+    stream=sys.stdout,
+    format="[%(asctime)s] [%(filename)s:%(lineno)d] %(levelname)s - %(message)s",
+    level=logging.DEBUG,
+    datefmt="%Y-%m-%d %H:%M:%S",
+)
+
+logger = logging.getLogger()
+
+if __name__ == "__main__":
+    logger.debug("Logging")
