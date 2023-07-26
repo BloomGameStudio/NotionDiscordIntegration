cdd2027d25d59e853fe682d8261d491cf322e9a6
Balu <baluproton+gitlab@protonmail.com>
2023-07-26 12:32:27 +0200

    Async Error Handling & Page and Name Support  for Page Title

--- utils/notion_utils.py
+++ utils/notion_utils.py
@@ -1,5 +1,6 @@
 from notion_client import Client
 import os
+from pprint import pprint
 
 notion = Client(auth=os.environ["NOTION_TOKEN"])
 
@@ -15,25 +16,40 @@ def get_username_by_id(id):
 
 
 def get_page_title(page):
-    _properties = page.get("properties")
-    # pprint(f"properties: {_properties}")
+    try:
+        _properties = page.get("properties")
+        # pprint(f"properties: {_properties}")
 
-    _Name = _properties.get("Page")
-    # pprint(f"Name:{_Name}")
+        try:
+            _Name = _properties.get("Name")
+            # pprint(f"Name:{_Name}")
+            _title = _Name.get("title")[0]
+            # pprint(f"_title:{_title}")
+        except AttributeError as e:
+            pass
 
-    _title = _Name.get("title")[0]
-    # pprint(f"_title:{_title}")
+        try:
+            _Page = _properties.get("Page")
+            # pprint(f"Page:{_Page}")
+            _title = _Page.get("title")[0]
+            # pprint(f"_title:{_title}")
+        except AttributeError as e:
+            pass
 
-    _plain_text = _title.get("plain_text")
-    # pprint(f"_plain_text:{_plain_text}")
+        _plain_text = _title.get("plain_text")
+        # pprint(f"_plain_text:{_plain_text}")
 
-    _text = _title.get("text")
-    # pprint(f"_text:{_text}")
+        _text = _title.get("text")
+        # pprint(f"_text:{_text}")
 
-    title = _text.get("content")
-    # pprint(f"title:{title}")
+        title = _text.get("content")
+        # pprint(f"title:{title}")
+        return title
 
-    return title
+    except:
+        pass
+
+    return "No Title Availabe"
 
 
 def get_page_plain_text_title(page):
