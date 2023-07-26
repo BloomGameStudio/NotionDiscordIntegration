ff22a3ed4d65edffe0b0795571663b8707a71111
Balu <baluproton+gitlab@protonmail.com>
2023-07-26 12:33:15 +0200

    notion db example response

---
+++ notion_db_example_response.py
@@ -0,0 +1,124 @@
+{
+    "archived": False,
+    "cover": None,
+    "created_by": {"id": "a72eb788-64cc-4c0f-9a11-58b6f340e60c", "object": "user"},
+    "created_time": "2023-03-30T20:25:00.000Z",
+    "description": [],
+    "icon": None,
+    "id": "d963d6a8-6de7-430e-a254-3f5c21c0acee",
+    "is_inline": True,
+    "last_edited_by": {"id": "a72eb788-64cc-4c0f-9a11-58b6f340e60c", "object": "user"},
+    "last_edited_time": "2023-07-23T23:47:00.000Z",
+    "object": "database",
+    "parent": {
+        "database_id": "07752fd5-ba8e-44c7-b8e4-8bfee50f0545",
+        "type": "database_id",
+    },
+    "properties": {
+        "Commitment": {
+            "id": "uOjz",
+            "name": "Commitment",
+            "number": {"format": "dollar"},
+            "type": "number",
+        },
+        "Contact": {"id": "E_bS", "name": "Contact", "people": {}, "type": "people"},
+        "Future $Hue": {
+            "id": "fOx%5C",
+            "name": "Future $Hue",
+            "number": {"format": "number"},
+            "type": "number",
+        },
+        "Name": {"id": "title", "name": "Name", "title": {}, "type": "title"},
+        "Phase": {
+            "id": "%3C%7CNa",
+            "multi_select": {
+                "options": [
+                    {"color": "brown", "id": "QJ={", "name": "Trailer-Seed"},
+                    {"color": "purple", "id": "SgmE", "name": "Pre-Seed-Maybe"},
+                    {"color": "green", "id": "PTMU", "name": "Pre-Seed"},
+                ]
+            },
+            "name": "Phase",
+            "type": "multi_select",
+        },
+        "Relevant Link": {
+            "id": "EJZH",
+            "name": "Relevant Link",
+            "rich_text": {},
+            "type": "rich_text",
+        },
+        "Status": {
+            "id": "ugNo",
+            "name": "Status",
+            "select": {
+                "options": [
+                    {
+                        "color": "green",
+                        "id": "60888b00-833a-49cf-a210-1201774dfd40",
+                        "name": "Done (Funded)",
+                    },
+                    {"color": "brown", "id": "{;OG", "name": "SAFT Signed"},
+                    {"color": "pink", "id": "lbS@", "name": "SAFT Sent"},
+                    {
+                        "color": "purple",
+                        "id": "93a9ea85-ec1e-4871-9702-f1214f0fdcda",
+                        "name": "Committed",
+                    },
+                    {"color": "yellow", "id": "\\QwV", "name": "In Flight"},
+                    {"color": "gray", "id": "F?Ot", "name": "Not Contacted"},
+                    {"color": "default", "id": "oQA]", "name": "IndirectContribution"},
+                    {"color": "red", "id": "?Umh", "name": "Declined (For " "Now)"},
+                ]
+            },
+            "type": "select",
+        },
+        "TX": {"id": "lnn%5B", "name": "TX", "rich_text": {}, "type": "rich_text"},
+        "TX Hash": {
+            "id": "%7DF%5Co",
+            "name": "TX Hash",
+            "rich_text": {},
+            "type": "rich_text",
+        },
+        "Twitter": {"id": "%40bhv", "name": "Twitter", "type": "url", "url": {}},
+        "Type": {
+            "id": "%3Ba%7DD",
+            "multi_select": {
+                "options": [
+                    {"color": "pink", "id": "D_yw", "name": "Grant"},
+                    {"color": "gray", "id": "DeSe", "name": "VC"},
+                    {"color": "green", "id": "zy~=", "name": "Individual " "Angel"},
+                    {
+                        "color": "purple",
+                        "id": "160bf833-96f7-4a9d-b9b6-e6607489f2be",
+                        "name": "Friend/Family",
+                    },
+                    {
+                        "color": "orange",
+                        "id": "35f8b7e7-d7dc-4f81-99ce-78f3d21bd3ac",
+                        "name": "Group",
+                    },
+                ]
+            },
+            "name": "Type",
+            "type": "multi_select",
+        },
+    },
+    "public_url": None,
+    "title": [
+        {
+            "annotations": {
+                "bold": False,
+                "code": False,
+                "color": "default",
+                "italic": False,
+                "strikethrough": False,
+                "underline": False,
+            },
+            "href": None,
+            "plain_text": "Funding Prospects",
+            "text": {"content": "Funding Prospects", "link": None},
+            "type": "text",
+        }
+    ],
+    "url": "https://www.notion.so/d963d6a86de7430ea2543f5c21c0acee",
+}
