1f2e4bd17fb1f4b82fdfb8f5fcb4cd944c5e333e
Balu <baluproton+gitlab@protonmail.com>
2023-07-26 12:30:53 +0200

    Discord Client & Async Background Task with Handle Updates

---
+++ bot.py
@@ -0,0 +1,34 @@
+import discord
+import os
+import asyncio
+from notion import handle_updates
+from my_logger import logger
+
+NOTION_UPDATE_NOTIFICATION_CHANNEL = 1130461410871226368
+
+
+class MyClient(discord.Client):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    async def setup_hook(self) -> None:
+        # create the background task and run it in the background
+        self.bg_tasks = self.loop.create_task(self.my_background_task())
+
+    async def on_ready(self):
+        logger.info(f"Logged in as {self.user} (ID: {self.user.id})")
+        logger.info("------")
+
+    async def my_background_task(self):
+        await self.wait_until_ready()
+        channel = self.get_channel(NOTION_UPDATE_NOTIFICATION_CHANNEL)
+        while not self.is_closed():
+            await handle_updates(channel)
+            await asyncio.sleep(1)  # task runs every x seconds
+
+
+intents = discord.Intents.default()
+
+client = MyClient(intents=intents)
+
+client.run(os.environ["DISCORD_BOT_TOKEN"])
