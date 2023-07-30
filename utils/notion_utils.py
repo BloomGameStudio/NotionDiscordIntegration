65fcc265465c7e1d7e6c1d8f380ca6657f04246d
Balu <baluproton+gitlab@protonmail.com>
2023-07-30 12:11:21 +0200

    RM Try block from page title to integrate with get page title

--- utils/notion_utils.py
+++ utils/notion_utils.py
@@ -28,40 +28,34 @@ async def get_username_by_id(id):
 
 
 def get_page_title(page):
+    _properties = page.get("properties")
+    # pprint(f"properties: {_properties}")
+
     try:
-        _properties = page.get("properties")
-        # pprint(f"properties: {_properties}")
-
-        try:
-            _Name = _properties.get("Name")
-            # pprint(f"Name:{_Name}")
-            _title = _Name.get("title")[0]
-            # pprint(f"_title:{_title}")
-        except AttributeError as e:
-            pass
-
-        try:
-            _Page = _properties.get("Page")
-            # pprint(f"Page:{_Page}")
-            _title = _Page.get("title")[0]
-            # pprint(f"_title:{_title}")
-        except AttributeError as e:
-            pass
-
-        _plain_text = _title.get("plain_text")
-        # pprint(f"_plain_text:{_plain_text}")
-
-        _text = _title.get("text")
-        # pprint(f"_text:{_text}")
-
-        title = _text.get("content")
-        # pprint(f"title:{title}")
-        return title
-
-    except:
+        _Name = _properties.get("Name")
+        # pprint(f"Name:{_Name}")
+        _title = _Name.get("title")[0]
+        # pprint(f"_title:{_title}")
+    except AttributeError as e:
         pass
 
-    return "No Title Available"
+    try:
+        _Page = _properties.get("Page")
+        # pprint(f"Page:{_Page}")
+        _title = _Page.get("title")[0]
+        # pprint(f"_title:{_title}")
+    except AttributeError as e:
+        pass
+
+    _plain_text = _title.get("plain_text")
+    # pprint(f"_plain_text:{_plain_text}")
+
+    _text = _title.get("text")
+    # pprint(f"_text:{_text}")
+
+    title = _text.get("content")
+    # pprint(f"title:{title}")
+    return title
 
 
 def get_page_plain_text_title(page):
