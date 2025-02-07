998c8ae760f9ee513592cb81dcf8fe791b0501da
sarahtonein <joemoe1899@gmail.com>
2025-02-07 02:41:51 +0000

    delete old unused / unecessary files after domain arch refactor

--- constants.py
+++
@@ -1,5 +0,0 @@
-COLLECTIVE_DB = "07752fd5ba8e44c7b8e48bfee50f0545?"
-DATE_FMT = "%d %m %Y %H:%M"
-
-# The Discord Channels to which to bot sends notifications
-NOTION_NOTIFICATION_CHANNELS: list[int] = [1126132159158173726, 1191205161310363709]
